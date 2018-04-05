7fef9bab30c82078283ed59b3fe18d1bda3f2418
maurolepore <maurolepore@gmail.com>
2018-04-05 17:03:32 -0400

    Use data.

--- R/data.R
+++ R/data.R
@@ -6,10 +6,20 @@
 "taxa_bci"
 
 
-#' * One hectare of data for hectare: px>=700 and px<800 and py>=100 and py<200.
-#' The data includes only those records from census 2005 and census 2010.
+
+#' All threes in one hectare from Barro Colorado Island..
+#'
+#' One hectare of data for hectare: `px >= 700` and `px < 800`; and `py >=100`
+#' and `py < 200`. The data includes only those records from census 2005 and
+#' census 2010.
+#'
+"vft_1ha_bci"
 
 
 
-#' * 1000 randomly chosen trees from the whole plot. Only included data from
-#' census 2005 and census 2010.
+#' One Thousand trees selected at random from Barro Colorado Island.
+#'
+#' One Thousand, randomly chosen, trees from Barro Colorado Island (from the
+#' whole plot). Only included data from census 2005 and census 2010.
+#'
+"vft_random_bci"
