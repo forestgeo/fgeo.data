09dd813cd3b12b4e929096f51ad9ca2b7439b608
maurolepore <maurolepore@gmail.com>
2018-04-05 17:28:13 -0400

    Compleate quick documentation of data/

--- R/data.R
+++ R/data.R
@@ -1,8 +1,10 @@
 #' Complete ViewTaxonomy table from Barro Colorado Island.
 #'
-#' Slightly cleaned version of a ViewTaxonomy table. For a raw .csv version see
-#' [http://bit.ly/fgeo-opendata-taxa-bci](http://bit.ly/fgeo-opendata-taxa-bci).
+#' This is a slightly cleaned version of a ViewTaxonomy table. For a raw .csv
+#' version see \url{http://bit.ly/fgeo-opendata-taxa-bci}.
 #'
+#' @examples
+#' str(taxa_bci)
 "taxa_bci"
 
 
@@ -13,6 +15,11 @@
 #' and `py < 200`. The data includes only those records from census 2005 and
 #' census 2010.
 #'
+#' This is a slightly cleaned version from a ViewFullTable table.
+#' For a raw .csv version see \url{http://bit.ly/fgeo-opendata-vft-1ha-bci}.
+#'
+#' @examples
+#' str(vft_1ha_bci)
 "vft_1ha_bci"
 
 
@@ -22,4 +29,17 @@
 #' One Thousand, randomly chosen, trees from Barro Colorado Island (from the
 #' whole plot). Only included data from census 2005 and census 2010.
 #'
+#' This is a slightly cleaned version from a ViewFullTable table.
+#' For a raw .csv version see url{http://bit.ly/fgeo-opendata-vft-random-bci}.
+#'
+#' @examples
+#' str(vft_random_bci)
 "vft_random_bci"
+
+
+
+#' Elevation data from Barro Colorado Island.
+#'
+#' @examples
+#' str(elevation_bci)
+"elevation_bci"
