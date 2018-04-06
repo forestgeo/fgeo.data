58d235659c5f7505ab9edcaa802daf4bc7070bc1
maurolepore <maurolepore@gmail.com>
2018-04-06 11:53:06 -0400

    Add infrastrucute to host private data only locally.

---
+++ R/data-tree-stem.R
@@ -0,0 +1,39 @@
+# Data Source -------------------------------------------------------------
+
+# Original files sent By Suzanne Lao. (Mauro's private email
+# https://goo.gl/WhFNqd).
+
+# From: Lao, Suzanne <LAOZ@si.edu>
+# Date: Thu, Jul 27, 2017 at 1:33 PM
+# Subject: RE: Data from BCI for examples
+# To: Mauro Lepore <maurolepore@gmail.com>
+# Cc: "Davies, Stuart J." <DaviesS@si.edu>
+#
+# I have shared a folder with you via dropbox ***[bcifiles]***. It includes:
+#
+# -          BCI elevation data
+#
+# -          BCI 2005 and 2010 stem data
+#
+# -          BCI 2005 and 2010 tree data
+#
+# -          BCI species file
+#
+# -          wood density file, which contains some errors, and will be updated
+# soon. However the values for BCI should be fine.
+#
+# -          a text file with the habitats per 20x20m quadrats, which were
+# determined by observation.
+#
+# However, habitats can also be determined by the slope, convexity, and
+# elevation. I am attaching a file I made ***[bci.quad.info.rdata]***, which is
+# quite old. You may want to redo this file, or maybe someone else has a better
+# one.
+#
+# The tree and stem data are the most updated ones that we have from Rick.
+# Please use a subset of this data only.
+#
+# Let me know if you have any questions.
+#
+# Cheers,
+# Suzanne
