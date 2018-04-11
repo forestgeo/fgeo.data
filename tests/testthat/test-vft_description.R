5d092f6c13748464cdd8977058a542480c7c210a
maurolepore <maurolepore@gmail.com>
2018-04-11 13:47:52 -0400

    Avoid error on travis for dependency not available.

--- tests/testthat/test-vft_description.R
+++ tests/testthat/test-vft_description.R
@@ -1,6 +1,8 @@
 context("test-vft_description.R")
 
 test_that("Documented columns are as expected", {
+  # Not importing fgeo.tool
+  skip_on_travis()
   expect_equal(
     fgeo.data::vft_description$column, names(fgeo.tool::type_vft())
   )
