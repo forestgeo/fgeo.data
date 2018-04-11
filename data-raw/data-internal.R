17743d1c8b87ce3ef8cb1de9956a3f5a518bc854
maurolepore <maurolepore@gmail.com>
2018-04-11 16:43:05 -0400

    Make unique_id dataset internal.

---
+++ data-raw/data-internal.R
@@ -0,0 +1,10 @@
+# Unique identifiers
+# Source: https://github.com/forestgeo/bciex/issues/10
+unique_id <- tibble::tribble(
+  ~table,           ~column,
+  "tree",          "TreeID",
+  "stem",          "StemID",
+  "ViewFullTable", "DBHID"
+)
+
+use_data(unique_id, overwrite = TRUE, internal = TRUE)
