a50a7dc85f3f546d62f78473315d1577e13781dc
maurolepore <maurolepore@gmail.com>
2018-04-11 14:03:44 -0400

    Skip tetst if fgeo.tool if not installed

--- tests/testthat/test-vft_description.R
+++ tests/testthat/test-vft_description.R
@@ -2,7 +2,7 @@ context("test-vft_description.R")
 
 test_that("Documented columns are as expected", {
   # Not importing fgeo.tool
-  skip_on_travis()
+  testthat::skip_if_not_installed("fgeo.tool")
   expect_equal(
     fgeo.data::vft_description$column, names(fgeo.tool::type_vft())
   )
