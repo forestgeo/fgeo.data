14a3e88f6a28b8a8642cc7754feda9ca3c2cad14
maurolepore <maurolepore@gmail.com>
2018-04-11 19:06:36 -0400

    Add data dictionary.

---
+++ R/data_dictionary.R
@@ -0,0 +1,17 @@
+#' Data Dictionary of the ForestGEO database.
+#' 
+#' This dataset is based on the Tree Database Data Dictionary available at 
+#' \url{http://ctfs.si.edu/Public/DataDict/data_dict.php}.
+#' 
+#' @source \url{http://ctfs.si.edu/Public/DataDict/data_dict.php}.
+#' 
+#' @examples 
+#' head(data_dictionary)
+#' 
+#' # Column names vary in subtle ways (e.g. SubSpeciesID vs. SubspeciesID)
+#' pattern <- "^subspeciesid$"
+#' matching <- grepl(pattern, data_dictionary$column, ignore.case = TRUE)
+#' data_dictionary[matching, ]
+#'
+"data_dictionary"
+
