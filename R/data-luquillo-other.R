ee98b460207e879259542d1257b22dfc02e4b4de
maurolepore <maurolepore@gmail.com>
2018-04-19 16:43:10 -0400

    Update site

--- R/data-luquillo-other.R
+++ R/data-luquillo-other.R
@@ -1,16 +1,19 @@
-#' SPecies table from Luquillo.
+#' Species table from Luquillo.
 #' 
+#' @examples 
+#' str(luquillo_species, give.attr = FALSE)
 "luquillo_species"
 
-
-#' Elevation data from Luquillo
+#' Elevation data from Luquillo.
 #'
 #' This dataset can be downloaded directly from
 #' \url{http://bit.ly/fgeo-data-luquillo-elevation}.
 #'
 #' @examples
-#' str(luquillo_elevation)
+#' str(luquillo_elevation, , give.attr = FALSE)
+#' 
 #' sapply(luquillo_elevation, class)
-#' str(luquillo_elevation$col)
+#' 
+#' str(luquillo_elevation$col, give.attr = FALSE)
 "luquillo_elevation"
 
