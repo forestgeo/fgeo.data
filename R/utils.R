a28aa5f5b85018205bfee60cb0b49e660f31a9c0
maurolepore <maurolepore@gmail.com>
2018-04-25 10:43:31 -0400

    Add utils

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,19 @@
+commas <- function(...) paste0(..., collapse = ", ")
+
+check_crucial_names <- function(x, nms) {
+  stopifnot(is.character(nms))
+  are_names_expected <- all(nms %in% names(x))
+  if (are_names_expected) {
+    return(invisible(x))
+  }
+  else {
+    stop("Ensure your data set has these variables:\n", paste0(nms, 
+      collapse = ", "), call. = FALSE)
+  }
+}
+
+
+
 #' Factory to filter dataset names in a package matching some class.
 #' @noRd
 str_datasets_class <- function(.f = purrr::keep) {
